822efc0dea727cfbe36d34371e87ff5d955ed7ad
storyfrom1982 <storyfrom1982@gmail.com>
2024-03-18 21:58:45 +0800

    create qt6 project

---
+++ qmlmodules.cmake
@@ -0,0 +1,15 @@
+if(NOT EXISTS ${CMAKE_SOURCE_DIR}/Qaterial)
+    # 如果项目不存在，则执行 git clone 命令
+    execute_process(
+        COMMAND git clone -b qt6.7 git@github.com:storyfrom1982/Qaterial.git
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        RESULT_VARIABLE result
+    )
+    if(result)
+        message(FATAL_ERROR "Failed to clone Qaterial")
+    endif()
+endif()
+
+
+add_subdirectory(Qaterial)
+target_link_libraries(Acceptit PRIVATE Qaterial)
